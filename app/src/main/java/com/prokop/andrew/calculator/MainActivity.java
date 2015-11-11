d34d892646099faef35a457b6a2e397b56c08b0f
jmleep <j.m.leep@gmail.com>
2015-11-11 15:47:35 -0600

    Added in icon and theme colors/ Removed unused FAB.

--- app/src/main/java/com/prokop/andrew/calculator/MainActivity.java
+++ app/src/main/java/com/prokop/andrew/calculator/MainActivity.java
@@ -1,8 +1,6 @@
 package com.prokop.andrew.calculator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -80,14 +78,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     private boolean editTextNotBlank() {
